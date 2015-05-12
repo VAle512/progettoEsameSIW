7dd6ca298be1f71191e1016a89f17ff4684735a8
Alessandro <ale.fog93@yahoo.it>
2015-05-12 17:09:05 +0200

    aggiunte classi Customer e Order

---
+++ src/it/uniroma3/progettoEsameSIW/db/Customer.java
@@ -0,0 +1,128 @@
+package it.uniroma3.progettoEsameSIW.db;
+
+import java.sql.Date;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Customer {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	@Column(nullable=false)
+	private String name;
+	@Column(nullable=false)
+	private String surname;
+	private Date birthDate;
+	private Date registrationDate;
+	private String address;
+	private String email;
+	@OneToMany
+	@JoinColumn(name = "id")
+	private List<Order> orders;
+	
+	
+	public Customer(String name, String surname, Date birthDate,
+			Date registrationDate, String address, String email) {
+		this.name = name;
+		this.surname = surname;
+		this.birthDate = birthDate;
+		this.registrationDate = registrationDate;
+		this.address = address;
+		this.email = email;
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getSurname() {
+		return surname;
+	}
+
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+
+	public Date getRegistrationDate() {
+		return registrationDate;
+	}
+
+
+	public void setRegistrationDate(Date registrationDate) {
+		this.registrationDate = registrationDate;
+	}
+
+
+	public String getAddress() {
+		return address;
+	}
+
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public List<Order> getOrders() {
+		return orders;
+	}
+
+
+	public void setOrders(List<Order> orders) {
+		this.orders = orders;
+	}
+	
+	
+	
+	
+	
+
+}
---
+++ src/it/uniroma3/progettoEsameSIW/db/Order.java
@@ -0,0 +1,91 @@
+package it.uniroma3.progettoEsameSIW.db;
+
+import java.sql.Date;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Order {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private Date openDate;
+	private Date closeDate;
+	private Date evasionDate;
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "id")
+	@Column(nullable = false)
+	private Customer customer;
+	@OneToMany(fetch =FetchType.EAGER)
+	private List<OrderLine> orderLines;
+	
+	public Order(Date openDate, Date closeDate, Date evasionDate,
+			Customer customer) {
+		this.openDate = openDate;
+		this.closeDate = closeDate;
+		this.evasionDate = evasionDate;
+		this.customer = customer;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Date getOpenDate() {
+		return openDate;
+	}
+
+	public void setOpenDate(Date openDate) {
+		this.openDate = openDate;
+	}
+
+	public Date getCloseDate() {
+		return closeDate;
+	}
+
+	public void setCloseDate(Date closeDate) {
+		this.closeDate = closeDate;
+	}
+
+	public Date getEvasionDate() {
+		return evasionDate;
+	}
+
+	public void setEvasionDate(Date evasionDate) {
+		this.evasionDate = evasionDate;
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
+	public List<OrderLine> getOrderLines() {
+		return orderLines;
+	}
+
+	public void setOrderLines(List<OrderLine> orderLines) {
+		this.orderLines = orderLines;
+	}
+	
+	
+
+	
+}
