6201fa7565f0bca18475e680c196b83769ef7ed3
valerio <valerio_cetorelli@yahoo.it>
2015-05-26 12:55:23 +0200

    modificato persistence per connettersi al database aws

--- src/it/uniroma3/progettoEsameSIW/controller/action/GetCatalog.java
+++ src/it/uniroma3/progettoEsameSIW/controller/action/GetCatalog.java
@@ -13,14 +13,14 @@ public class GetCatalog implements Action {
 	@Override
 	public String perform(HttpServletRequest request) {
 
-//		ProductFacade facade = new ProductFacade();
-//		List<Product> listP = facade.getAllProducts();
+		ProductFacade facade = new ProductFacade();
+		List<Product> listP = facade.getAllProducts();
 		
-		Product p1 = new Product("nome1", "code1","desc1",new Double(1),new Integer(1));
-		Product p2 = new Product("nome2", "code2","desc2",new Double(2),new Integer(2));
-		List<Product> listP = new LinkedList<Product>();
-		listP.add(p1);
-		listP.add(p2);
+//		Product p1 = new Product("nome1", "code1","desc1",new Double(1),new Integer(1));
+//		Product p2 = new Product("nome2", "code2","desc2",new Double(2),new Integer(2));
+//		List<Product> listP = new LinkedList<Product>();
+//		listP.add(p1);
+//		listP.add(p2);
 		
 		request.setAttribute("catalog", listP);
 		return "/catalog.jsp";
