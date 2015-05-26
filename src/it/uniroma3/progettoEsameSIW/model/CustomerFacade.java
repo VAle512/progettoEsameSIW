656d0db5123e327f42df6be9c2f4ff2c83293c35
Alessandro <ale.fog93@yahoo.it>
2015-05-26 13:26:53 +0200

    modificati CustomerFacade e ProductFacade

--- src/it/uniroma3/progettoEsameSIW/model/CustomerFacade.java
+++ src/it/uniroma3/progettoEsameSIW/model/CustomerFacade.java
@@ -1,46 +1,31 @@
 package it.uniroma3.progettoEsameSIW.model;
 
-
 import java.util.Date;
+import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.persistence.PersistenceContext;
 
+@Stateless
 public class CustomerFacade {
 	
-	private EntityManagerFactory emf;
+	@PersistenceContext(unitName = "dbProgettoSIW-unit")
 	private EntityManager em;
 
-	public CustomerFacade()	{
-		this.emf = Persistence.createEntityManagerFactory("dbProgettoSIW-unit");
-		this.em = this.emf.createEntityManager();
-	}
-	
-	private void close(){
-		this.em.close();
-		this.emf.close();
-	}
 	
 	public Customer createCustomer(String name, String surname, Date birthDate,
 			Date registrationDate, Address address, String email, String userID, String password)	{
 		
 		Customer c = new Customer(name, surname, birthDate, registrationDate, address, email, userID, password);
-		EntityTransaction tx = this.em.getTransaction();
-		tx.begin();
 		this.em.persist(c);
-		tx.commit();
-		this.close();
 		return c;
 	}
 	
 	public Customer getCustomer(Long id)	{
 		Customer c = this.em.find(Customer.class, id);
-		this.close();
 		return c;
 	}
 	
-	public void deleteCustomer(Long id)		{
+/*	public void deleteCustomer(Long id)		{
 		EntityTransaction tx = this.em.getTransaction();
 		tx.begin();
 		Customer c = this.em.find(Customer.class, id);
@@ -55,5 +40,5 @@ public class CustomerFacade {
 		this.em.merge(c);
 		tx.commit();
 		this.close();
-	}
-}
+	} */
+}  
