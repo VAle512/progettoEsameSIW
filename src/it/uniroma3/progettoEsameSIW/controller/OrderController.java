61bfcfa47b651421fca2fc9348ec59950b9a98a0
valerio <valerio_cetorelli@yahoo.it>
2015-05-29 14:28:57 +0200

    modificato Order, aggiunto OrderFacade, creazione OrderController in corso

---
+++ src/it/uniroma3/progettoEsameSIW/controller/OrderController.java
@@ -0,0 +1,48 @@
+package it.uniroma3.progettoEsameSIW.controller;
+
+import it.uniroma3.progettoEsameSIW.model.Customer;
+import it.uniroma3.progettoEsameSIW.model.Order;
+import it.uniroma3.progettoEsameSIW.model.OrderFacade;
+import it.uniroma3.progettoEsameSIW.model.OrderLine;
+import it.uniroma3.progettoEsameSIW.model.Product;
+import it.uniroma3.progettoEsameSIW.model.ProductFacade;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.annotation.ManagedBean;
+import javax.ejb.EJB;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.context.FacesContext;
+import javax.persistence.Column;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+@ManagedBean
+@SessionScoped
+public class OrderController {
+	
+	@ManagedProperty(value="#{param.id}")
+	private Long id;
+	private Date openDate;
+	private Date closeDate;
+	private Date evasionDate;
+	private Customer customer;
+	private Order order;
+	private List<OrderLine> orderLines;
+	private Integer status;
+	
+	@EJB(name="oFAcade")
+	private OrderFacade orderFacade;
+	
+	public String createOrder() {
+		HttpServletRequest request = (HttpServletRequest)FacesContext.getCurrentInstance().getExternalContext().getRequest();
+		this.order = orderFacade.createOrder(new Date(), customer)
+	
+}
+}
---
+++ src/it/uniroma3/progettoEsameSIW/model/OrderFacade.java
@@ -0,0 +1,20 @@
+package it.uniroma3.progettoEsameSIW.model;
+
+import java.util.Date;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Stateless(name = "oFacade")
+public class OrderFacade {
+	
+	@PersistenceContext(unitName = "dbProgettoSIW-unit")
+	private EntityManager em;
+	
+	public Order createOrder(Date openDate, Customer customer){
+		Order o = new Order(openDate, customer);
+		this.em.persist(o);
+		return o;
+	}
+}
